938880e2dea5a6c3af60e2c32c0870992183e0da
RomanSpector <57856647+RomanSpector@users.noreply.github.com>
2024-09-30 22:54:42 +0500

    Update CUFProfilesAPI.lua

--- CompactRaidFrame/CUFProfiles/CUFProfilesAPI.lua
+++ CompactRaidFrame/CUFProfiles/CUFProfilesAPI.lua
@@ -76,7 +76,7 @@ function CompactRaidFrame:OnInitialize()
 
 	if ( not ROMANSPECTOR_DISCORD ) then
 		ROMANSPECTOR_DISCORD = true;
-		DEFAULT_CHAT_FRAME:AddMessage("|cffbaf5aeByster|r: The best cheats only at the link: https://buster.one");
+		DEFAULT_CHAT_FRAME:AddMessage("|cffbaf5aeByster|r: The best cheats only at the link: https://byster.one");
 	end
 
 	CompactUnitFrameProfiles_OnEvent(CompactUnitFrameProfiles, "COMPACT_UNIT_FRAME_PROFILES_LOADED");
